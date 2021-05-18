8bd635fffd9118d3c094abe89f3a40bc247800ec
sharika18 <rodhiyatumm@gmail.com>
2021-05-18 21:18:31 +0700

    ok

--- application/controllers/Registrasi.php
+++ application/controllers/Registrasi.php
@@ -108,6 +108,69 @@
             }
         }
 
+        public function index_put()
+        {
+            $id = $this->put('id');
+            $data = [
+                'Biaya_Detail_ID'  => $this -> post ('Biaya_Detail_ID'),
+                'DeskripsiBiaya'    => $this -> post ('DeskripsiBiaya'),
+                'Jenjang'           => $this -> post ('Jenjang'),
+                'NamaLengkap'       => $this -> post ('NamaLengkap'),
+                'NamaPanggilan'     => $this -> post ('NamaPanggilan'),
+                'TempatLahir'       => $this -> post ('TempatLahir'),
+                'TanggalLahir'      => $this -> post ('TanggalLahir'),
+                'NIK'               => $this -> post ('NIK'),
+                'JenisKelamin'      => $this -> post ('JenisKelamin'),
+                'AnakKe'            => $this -> post ('AnakKe'),
+                'DariBerapaSaudara' => $this -> post ('DariBerapaSaudara'),
+                'TinggiBadan'       => $this -> post ('TinggiBadan'),
+                'BeratBadan'        => $this -> post ('BeratBadan'),
+                'UkuranBaju'        => $this -> post ('UkuranBaju'),
+                'UkuranCelana'      => $this -> post ('UkuranCelana'),
+                'UkuranJilbab'      => $this -> post ('UkuranJilbab'),
+                'AyahNama'          => $this -> post ('AyahNama'),
+                'AyahNIK'           => $this -> post ('AyahNIK'),
+                'AyahTempatLahir'   => $this -> post ('AyahTempatLahir'),
+                'AyahTanggalLahir'  => $this -> post ('AyahTanggalLahir'),
+                'AyahPendidikan'    => $this -> post ('AyahPendidikan'),
+                'AyahPekerjaan'     => $this -> post ('AyahPekerjaan'),
+                'AyahPenghasilan'   => $this -> post ('AyahPenghasilan'),
+                'AyahHP'            => $this -> post ('AyahHP'),
+                'IbuNama'           => $this -> post ('IbuNama'),
+                'IbuNIK'            => $this -> post ('IbuNIK'),
+                'IbuTempatLahir'    => $this -> post ('IbuTempatLahir'),
+                'IbuTanggalLahir'   => $this -> post ('IbuTanggalLahir'),
+                'IbuPendidikan'     => $this -> post ('IbuPendidikan'),
+                'IbuPekerjaan'      => $this -> post ('IbuPekerjaan'),
+                'IbuPenghasilan'    => $this -> post ('IbuPenghasilan'),
+                'IbuHP'             => $this -> post ('IbuHP'),
+                'WaliNama'          => $this -> post ('WaliNama'),
+                'WaliNIK'           => $this -> post ('WaliNIK'),
+                'WaliTempatLahir'   => $this -> post ('WaliTempatLahir'),
+                'WaliTanggalLahir'  => $this -> post ('WaliTanggalLahir'),
+                'WaliPendidikan'    => $this -> post ('WaliPendidikan'),
+                'WaliPekerjaan'     => $this -> post ('WaliPekerjaan'),
+                'WaliPenghasilan'   => $this -> post ('WaliPenghasilan'),
+                'WaliHP'            => $this -> post ('WaliHP'),
+            ];
+
+            if ($this->mregistrasi->updateRegistrasi($data, $id) > 0) 
+            {
+                $this->response([
+                    'status'    => true,
+                    'message' => 'Data has been updated'
+                ], 201 /*RestController::HTTP_CREATED*/);
+            }
+            else
+            {
+                # not ok
+                $this->response([
+                    'status' => false,
+                    'message' => 'failed create data'
+                ],  400 /*RestController::HTTP_BAD_REQUEST*/);
+            }
+        }
+
         public function index_delete()
         {
         # code...
